5fce8a90c79dfae5e17fbb861068cc0d2d07fa8a
Rasmus Kuschel <rasmus@kuschel.it>
2017-01-06 16:46:48 +0100

    parse spec

--- app/controllers/SchemaController.scala
+++ app/controllers/SchemaController.scala
@@ -12,11 +12,10 @@ import services.SchemaService
 class SchemaController @Inject() extends Controller {
 
   val schemaService = new SchemaService
+  val schema: Schema = schemaService.loadSchema
 
   def index = Action {
-    val schema: Schema = schemaService.loadSchema
-
-    // some mock spec
+    // TODO some mock spec
     val spec = Spec(Seq(
       SpecField("gxl_agegroup_1", "Alter_Usage_IPAN_2008",
         direct = false,
@@ -29,17 +28,8 @@ class SchemaController @Inject() extends Controller {
     Ok(views.html.schema(schema, spec))
   }
 
-  def generate = Action {
-    // TODO Spec parsen
-    val spec = Spec(Seq(
-      SpecField("gxl_agegroup_1", "Alter_Usage_IPAN_2008",
-        direct = false,
-        mapping = Map(
-          " 0 bis 2 Jahre" -> "01",
-          " 3 bis 5 Jahre" -> "02"
-        ))
-    ))
-
+  def generate = Action { implicit request =>
+    val spec = Spec.fromRequest(request)
     Ok(views.txt.dep_schema_mapping(spec))
   }
 }
--- app/domain/Spec.scala
+++ app/domain/Spec.scala
@@ -1,24 +1,66 @@
 package domain
 
+import play.api.mvc.{AnyContent, Request}
+
 case class SpecField(
                       target: String, source: String = "",
                       direct: Boolean = true,
                       mapping: Map[String, String] = Map[String, String](),
-                      individual: Boolean = true, household: Boolean = false,
-                      yearly: Boolean = true, variable: Boolean = false,
+                      individual: Boolean = true,
+                      yearly: Boolean = true,
                       atr: Boolean = true, atlas: Boolean = false,
-                      evogenius: Boolean = false, survey: Boolean = false)
+                      evogenius: Boolean = false, survey: Boolean = false) {
+
+  val origin: String = if (individual) "P" else "HH"
+
+  val household: Boolean = !individual
+  val variable: Boolean = !yearly
+}
 
 case class Spec(specFields: Seq[SpecField]) {
 
   def validate(schema: Schema): Boolean = ???
+  def save = ???
+  def load = ???
 
   def forField(field: Field): SpecField =
     specFields.
       find(_.target == field.name).
       getOrElse(SpecField(field.name))
+
+  def featureSelect: String = specFields.map(specField =>
+      s"featurename == '${specField.source}' AND origin == '${specField.origin}'"
+    ).mkString(" OR ")
 }
 
 object Spec {
   val emptySpec = Spec(Nil)
+
+  def fromRequest(request: Request[AnyContent]): Spec = {
+
+    val rawData = request.body.asFormUrlEncoded.getOrElse(Map[String, Seq[String]]())
+
+    val fieldNames: Seq[String] = rawData.keySet.
+      filter(_.startsWith("origin_")).
+      map(_.substring("origin_".length)).toSeq
+
+    val specFields = fieldNames.map(fieldName => {
+      def extract(prefix: String): String = rawData.get(prefix + "_" + + fieldName).flatMap(_.headOption).getOrElse("")
+
+      SpecField(
+        target = fieldName,
+        source = extract("source"),
+        direct = extract("direct").equalsIgnoreCase("on"),
+        // mapping: Map[String, String] = Map[String, String](),
+        individual = extract("origin").equalsIgnoreCase("Individual"),
+        yearly = extract("frequency").equalsIgnoreCase("yearly"),
+        atr = extract("src").equalsIgnoreCase("atr"),
+        atlas = extract("src").equalsIgnoreCase("atlas"),
+        evogenius = extract("src").equalsIgnoreCase("evogenius"),
+        survey = extract("src").equalsIgnoreCase("survey")
+      )
+    })
+
+    Spec(specFields)
+  }
 }
